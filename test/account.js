eeadae091ce6f8cd648464ff61d443da55f664b5
Salama AB <aksalj@aksalj.com>
2016-01-19 10:48:15 +0300

    Prepare & test sms send & fetch

---
+++ test/account.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var should = require('should');
+var validate = require('validate.js');
+var fixtures = require('./fixtures');
+
+describe('Account', function () {
+    this.timeout(5000);
+
+    it('fetched account info', function (done) {
+
+        var AfricasTalking = require('../lib')(fixtures.TEST_ACCOUNT);
+
+        var p = AfricasTalking.fetchAccount();
+        validate.isPromise(p).should.be.exactly(true);
+
+        p.then(function (resp) {
+            resp.should.have.property('UserData');
+            done();
+
+        }).catch(function (error) {
+            should.not.exist(error);
+            done();
+        });
+
+    });
+});
