4a22eecbb8ce7d40c74bda5121761a62f6c90ab6
Jodom <jodomouma@gmail.com>
2018-08-17 14:11:37 +0300

    test actions with response builder

--- test/voice.js
+++ test/voice.js
@@ -4,9 +4,11 @@ const should   = require('should');
 const validate = require('validate.js');
 const fixtures = require('./fixtures.local');
 
-var AfricasTalking, voice;
+let AfricasTalking, voice, response;
 
-describe('Voice', function () {
+let responseTemplate = '<?xml version=\"1.0\" encoding=\"UTF-8\"?><Response>';
+
+describe('response', function () {
 
     this.timeout(15000);
 
@@ -15,282 +17,346 @@ describe('Voice', function () {
         voice = AfricasTalking.VOICE;
     });
 
+    beforeEach(function () {
+        response = new voice.ActionBuilder;
+    })
+
     describe('Actions', function () {
-        describe('Say', function () {
-            let options = {};
-            let expected;
+        describe('say', function () {
+            let expected, options;
 
             describe('validation', function () {
                 it('<Say> cannot be empty', function () {
-                    (function () { voice.Say({}) })
+                    (function () { response.say() })
                         .should.throw(Error);
                 });
             });
 
             it('creates <Say> with text only', function () {
-                options.text = 'Hello there';
-                expected = '<Say>Hello there</Say>';
+                expected = responseTemplate +
+                    '<Say>Hello there</Say>' +
+                    '</Response>';
 
-                voice.Say(options)
+                response.say('Hello there')
+                    .build()
                     .should.equal(expected);
             });
 
             it('creates <Say> with text, optional parameters', function () {
-                Object.assign(options, {
-                    voice: 'woman',
+                options = {
+                    response: 'woman',
                     playBeep: true
-                });
-                expected = '<Say voice="woman" playBeep="true">Hello there</Say>';
+                };
+                expected = responseTemplate +
+                    '<Say response="woman" playBeep="true">Hello there</Say>' +
+                    '</Response>';
 
-                voice.Say(options)
+                response.say('Hello there', options)
+                    .build()
                     .should.equal(expected);
             });
         });
 
-        describe('Play', function () {
-            let options = {};
+        describe('play', function () {
             let expected;
 
             describe('validation', function () {
                 it('<Play> must have url', function () {
-                    (function () { voice.Play({}) })
+                    (function () { response.play() })
                         .should.throw(Error);
                 });
             });
 
             it('creates valid <Play>', function () {
-                options.url = 'http://www.myvoicemailserver.com/audio/vmail.wav';
-                expected = '<Play url="http://www.myvoicemailserver.com/audio/vmail.wav"/>';
+                expected = responseTemplate +
+                '<Play url="http://www.myresponsemailserver.com/audio/vmail.wav"/>' +
+                '</Response>';
 
-                voice.Play(options)
+                response.play('http://www.myresponsemailserver.com/audio/vmail.wav')
+                    .build()
                     .should.equal(expected);
             });
         });
 
-        describe('GetDigits', function () {
-            let options = {};
-            let expected;
+        describe('getDigits', function () {
+            let expected, child;
 
             describe('validation', function () {
                 it('<GetDigits> must have <Say> or <Play> child', function () {
-                    (function () { voice.GetDigits({}) })
+                    (function () { response.getDigits() })
                         .should.throw(Error);
                 });
 
                 it('cannot have invalid child', function () {
-                    options.text = voice.Dial({ phoneNumbers: '+254711XXXYYY' });
+                    let options = {
+                        dial: { phoneNumbers: '+254711XXXYYY' }
+                    }
 
-                    (function () { voice.GetDigits(options) })
+                    (function () { response.getDigits(options).build() })
                         .should.throw(Error);
                 });
             });
 
             it('creates <GetDigits> with <Play> child', function () {
-                options.text = voice.Play({ url: "http://www.myvoicemailserver.com/audio/vmail.wav" });
-                expected = '<GetDigits><Play url="http://www.myvoicemailserver.com/audio/vmail.wav"/></GetDigits>';
+                child = {
+                    play: { url: 'http://www.myresponsemailserver.com/audio/vmail.wav' }
+                }
+                expected = responseTemplate +
+                '<GetDigits><Play url="http://www.myresponsemailserver.com/audio/vmail.wav"/></GetDigits>' +
+                '</Response>';
 
-                voice.GetDigits(options)
+                response.getDigits(child)
+                    .build()
                     .should.equal(expected);
             });
 
             it('creates <GetDigits> with <Say> child', function () {
-                options.text = voice.Say({ text: "Hello" });
-                expected = '<GetDigits numDigits="1" ><Say>Hello</Say></GetDigits>';
+                child = {
+                    say: { text: 'Hello there' }
+                }
+                expected = responseTemplate +
+                '<GetDigits numDigits="1" ><Say>Hello there</Say></GetDigits>' +
+                '</Response>';
 
-                voice.GetDigits(options)
+                response.getDigits(child)
+                    .build()
                     .should.equal(expected);
             });
 
             it('creates <GetDigits> with <Say> child, optional parameters', function () {
-                Object.assign(options, {
+                options = {
                     numDigits: 1,
                     timeout: 30,
                     finishOnKey: "#",
                     callbackUrl: "http://myapp.com/callback"
-                });
-                expected = '<GetDigits numDigits="1" timeout="30" finishOnKey="#" \
-            callbackUrl="http://myapp.com/callback" ><Say>Hello</Say></GetDigits>';
+                }
+                expected = responseTemplate +
+                '<GetDigits numDigits="1" timeout="30" finishOnKey="#" ' +
+                'callbackUrl="http://myapp.com/callback" ><Say>Hello</Say></GetDigits>' +
+                '</Response>';
 
-                voice.GetDigits(options)
+                response.getDigits(options)
+                    .build()
                     .should.equal(expected);
             });
         });
 
-        describe('Dial', function () {
-            let options = {};
-            let expected;
+        describe('dial', function () {
+            let expected, options;
 
             describe('validation', function () {
                 it('<Dial> must have phoneNumbers', function () {
-                    (function () { voice.Dial({}) })
+                    (function () { response.dial() })
                         .should.throw(Error);
                 });
             });
 
             it('creates <Dial> with phoneNumbers', function () {
-                options.phoneNumbers = '+254711XXXYYY,+254733YYYZZZ,test@ke.sip.africastalking.com';
-                expected = '<Dial phoneNumbers="+254711XXXYYY,+254733YYYZZZ,test@ke.sip.africastalking.com"/>'
+                expected = responseTemplate +
+                '<Dial phoneNumbers="+254711XXXYYY,+254733YYYZZZ,test@ke.sip.africastalking.com"/>' +
+                '</Response>';
 
-                voice.Dial(options)
+                response.dial('+254711XXXYYY,+254733YYYZZZ,test@ke.sip.africastalking.com')
+                    .build()
                     .should.equal(expected);
             });
 
             it('creates <Dial> with phoneNumbers, optional params', function () {
-                Object.assign(options, {
+                options = {
                     record: true,
                     sequential: true,
                     callerId: '+254711222333',
                     ringBackTone: 'http://mymediafile.com/playme.mp3',
                     maxDuration: 120
-                });
-                expected = '<Dial phoneNumbers="+254711XXXYYY,+254733YYYZZZ,test@ke.sip.africastalking.com" ' +
-                'record="true" sequential="true" callerId="+254711222333" ringBackTone="http://mymediafile.com/playme.mp3" maxDuration="120"/>'
+                }
+                expected = responseTemplate +
+                '<Dial phoneNumbers="+254711XXXYYY,+254733YYYZZZ,test@ke.sip.africastalking.com" ' +
+                'record="true" sequential="true" callerId="+254711222333" ' +
+                'ringBackTone="http://mymediafile.com/playme.mp3" maxDuration="120"/>' +
+                '</Response>';
 
-                voice.Dial(options)
+                response.dial('+254711XXXYYY,+254733YYYZZZ,test@ke.sip.africastalking.com', options)
+                    .build()
                     .should.equal(expected);
             });
         });
 
-        describe('Record', function () {
-            let options = {};
-            let expected;
+        describe('record', function () {
+            let expected, child, options;
 
             describe('validation', function () {
                 it('<Record> must have <Say> or <Play> child', function () {
-                    (function () { voice.Record({}) })
+                    (function () { response.record() })
                         .should.throw(Error);
                 });
 
                 it('cannot have invalid child', function () {
-                    options.text = voice.Dial({ phoneNumbers: '+254711XXXYYY' });
+                    child = {
+                        dial: { phoneNumbers: '+254711XXXYYY' }
+                    }
 
-                    (function () { voice.Record(options) })
+                    (function () { response.record(child).build() })
                         .should.throw(Error);
                 });
             });
 
             it('creates <Record> with <Play> child', function () {
-                options.text = voice.Play({ url: "http://www.myvoicemailserver.com/audio/vmail.wav" });
-                expected = '<Record><Play url="http://www.myvoicemailserver.com/audio/vmail.wav"/></Record>';
+                child = {
+                    play: { url: 'http://www.myresponsemailserver.com/audio/vmail.wav' }
+                }
+                expected = responseTemplate +
+                '<Record><Play url="http://www.myresponsemailserver.com/audio/vmail.wav"/></Record>' +
+                '</Response>';
 
-                voice.Record(options)
+                response.record(child)
+                    .build()
                     .should.equal(expected);
             });
 
             it('creates <Record> with <Say> child', function () {
-                options.text = voice.Say({ text: "Hello" });
-                expected = '<Record><Say>Hello</Say></Record>';
+                child = {
+                    say: { text: 'Hello there' }
+                }
+                expected = responseTemplate +
+                '<Record><Say>Hello</Say></Record>' +
+                '</Response>'
 
-                voice.Record(options)
+                response.record(child)
+                    .build()
                     .should.equal(expected);
             });
 
             it('creates <Record> with optional parameters', function () {
-                Object.assign(options, {
+                child = {
+                    say: { text: 'Hello there' }
+                }
+                options = {
                     finishOnKey: "#",
                     maxLength: 120,
                     timeout: 3600,
                     trimSilence: true,
                     playBeep: true,
                     callbackUrl: "http://myapp.com/callback"
-                });
+                }
                 expected = '<Record finishOnKey="#" maxLengh="120" timeout="3600" \
             trimSilence="true" playBeep="true" callBackUrl="http://myapp.com/callback">\
             <Say>Hello</Say></Record>';
 
-                voice.Record(options)
+                response.record(child, options)
+                    .build()
                     .should.equal(expected);
             });
         });
 
-        describe('Enqueue', function () {
-            let options = {};
-            let expected;
+        describe('enqueue', function () {
+            let expected, options;
 
             it('creates valid <Enqueue>', function () {
-                expected = '<Enqueue/>';
+                expected = responseTemplate +
+                '<Enqueue/>' +
+                '</Response>';
 
-                voice.Enqueue(options)
+                response.enqueue()
+                    .build()
                     .should.equal(expected);
             });
 
             it('creates <Enqueue> with optional parameters', function () {
-                options.holdMusic = 'http://www.mymediaserver.com/audio/callWaiting.wav';
-                options.name = 'support';
-                expected = '<Enqueue holdMusic="http://www.mymediaserver.com/audio/callWaiting.wav" name="support"/>';
+                options = {
+                    holdMusic: 'http://www.mymediaserver.com/audio/callWaiting.wav',
+                    name: 'support'
+                }
+                expected = responseTemplate +
+                '<Enqueue holdMusic="http://www.mymediaserver.com/audio/callWaiting.wav" name="support"/>' +
+                '</Response>';
 
-                voice.Enqueue(options)
+                response.enqueue(options)
+                    .build()
                     .should.equal(expected);
             });
         });
 
-        describe('Dequeue', function () {
-            let options = {};
-            let expected;
+        describe('dequeue', function () {
+            let expected, options;
 
             describe('validation', function () {
                 it('<Dequeue> must have a phoneNumber', function () {
-                    (function () { voice.Dequeue({}) })
+                    (function () { response.dequeue() })
                         .should.throw(Error);
                 });
             });
 
             it('creates <Dequeue> with phoneNumber', function () {
-                options.phoneNumber = '+254711222333';
-                expected = '<Dequeue phoneNumber="+254711222333"/>';
+                options = {
+                    phoneNumber: '+254711222333'
+                }
+                expected = responseTemplate +
+                '<Dequeue phoneNumber="+254711222333"/>' +
+                '</Response>';
 
-                voice.Dequeue(options)
+                response.dequeue(options)
+                    .build()
                     .should.equal(expected);
             });
 
             it('creates <Dequeue> with phoneNumber and name', function () {
-                options.name = 'support';
-                expected = '<Dequeue phoneNumber="+254711222333" name="support"/>';
+                options = {
+                    phoneNumber: '+254711222333',
+                    name: 'support'
+                }
+                expected = responseTemplate +
+                '<Dequeue phoneNumber="+254711222333" name="support"/>' +
+                '</Response>'
 
-                voice.Dequeue(options)
+                response.dequeue(options)
+                    .build()
                     .should.equal(expected);
             });
         });
 
         describe('Redirect', function () {
-            let options = {};
             let expected;
 
             describe('validation', function () {
                 it('<Redirect> cannot be empty', function () {
-                    (function () { voice.Redirect({}) })
+                    (function () { response.redirect() })
                         .should.throw(Error);
                 });
 
                 it('<Redirect> text must be valid url', function () {
-                    options.text = 'Hello is not url';
 
-                    (function () { voice.Redirect(options) })
+                    (function () { response.redirect('Hello is not url') })
                         .should.throw(Error);
                 });
             });
 
             it('creates valid <Redirect> given url', function () {
-                options.text = 'http://myredirecturl.com/rdrct';
-                expected = '<Redirect>http://myredirecturl.com/rdrct</Redirect>'
+                expected = responseTemplate +
+                '<Redirect>http://myredirecturl.com/rdrct</Redirect>' +
+                '</Response>'
 
-                voice.Redirect(options)
+                response.redirect('http://myredirecturl.com/rdrct')
+                    .build()
                     .should.equal(expected);
             });
         });
 
         describe('Conference', function () {
-            let expected = '<Conference/>'
+            let expected = responseTemplate + '<Conference/></Response>';
+
             it('creates valid <Conference>', function () {
-                voice.Conference()
+                response.conference()
+                    .build()
                     .should.equal(expected);
             });
         });
 
         describe('Reject', function () {
-            let expected = '<Reject/>'
+            let expected = responseTemplate + '<Reject/></Response>';
+
             it('creates valid <Reject>', function () {
-                voice.Reject()
+                response.reject()
+                    .build()
                     .should.equal(expected);
             });
         });
