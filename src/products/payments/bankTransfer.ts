9ebdce8c81e36bfd6b954d420db4ff3750124d95
tawn33y <tawn33y@gmail.com>
2019-12-18 16:05:25 +0300

    [feature/typescript] Add bankTransfer

---
+++ src/products/payments/bankTransfer.ts
@@ -0,0 +1,43 @@
+import joi from 'joi';
+import { Credentials } from '../../utils/getCredentials.interface';
+import { BankTransferOptions, BankTransferResponse, BankTransferPostData } from './bankTransfer.interface';
+import { getFullCredentials } from '../../utils/getCredentials';
+import { validateJoiSchema, sendRequest } from '../../utils/misc';
+
+const getSchema = () => joi.object({
+  productName: joi.string().regex(/\S/, 'no space').required(),
+  recipients: joi.array().items(
+    joi.object({
+      bankAccount: joi.object({
+        accountName: joi.string().required(),
+        accountNumber: joi.string().required(),
+        bankCode: joi.number().required(),
+        dateOfBirth: joi.string(),
+      }).required(),
+      currencyCode: joi.string().valid('KES', 'UGX', 'USD').required(),
+      amount: joi.number().required(),
+      narration: joi.string().regex(/\S/, 'no space').required(),
+      metadata: joi.object(),
+    }).required(),
+  ).min(1).required(),
+}).required();
+
+export const bankTransfer = (credentials: Credentials) => async (
+  options: BankTransferOptions,
+): Promise<BankTransferResponse> => {
+  const { apiKey, username, format } = await getFullCredentials(credentials);
+  const result = await validateJoiSchema<BankTransferOptions>(getSchema(), options);
+
+  const postData: BankTransferPostData = {
+    ...result,
+    username,
+  };
+
+  return sendRequest<BankTransferResponse, BankTransferPostData>('BANK_TRANSFER', username, 'POST', postData, {
+    headers: {
+      apiKey,
+      accept: format,
+      'Content-Type': 'application/json',
+    },
+  });
+};
--- src/products/payments/index.ts
+++ src/products/payments/index.ts
@@ -4,6 +4,7 @@ import { mobileB2C } from './mobileB2C';
 import { mobileB2B } from './mobileB2B';
 import { bankCheckoutCharge } from './bankCheckoutCharge';
 import { bankCheckoutValidate } from './bankCheckoutValidate';
+import { bankTransfer } from './bankTransfer';
 
 export const PAYMENTS = (credentials: Credentials) => ({
   mobileCheckout: mobileCheckout(credentials),
@@ -11,6 +12,7 @@ export const PAYMENTS = (credentials: Credentials) => ({
   mobileB2B: mobileB2B(credentials),
   bankCheckoutCharge: bankCheckoutCharge(credentials),
   bankCheckoutValidate: bankCheckoutValidate(credentials),
+  bankTransfer: bankTransfer(credentials),
 
   // fallbacks
   checkout: mobileCheckout(credentials),
