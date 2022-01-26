c53aff3a834ff3b6095ed98ffb2d55026cfaf151
Newton Mbugua <mymbugua@gmail.com>
2022-01-26 22:00:21 +0300

    send multiple messages to multiple recipients with the SMS.send() method

--- test/fixtures.js
+++ test/fixtures.js
@@ -1,8 +1,8 @@
 'use strict';
 
 exports.TEST_ACCOUNT = {
-    apiKey: "YOUR_API_KEY",
-    username: "YOUR_USERNAME",
-    format: "json"
+  apiKey: 'YOUR_API_KEY',
+  username: 'YOUR_USERNAME',
+  format: 'json',
 };
 exports.phoneNumber = '+2547xxxxxxxx';
