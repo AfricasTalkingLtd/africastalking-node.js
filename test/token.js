d3a8078c034c312103ba635937c9bbd3d09da96a
mercie-ux <mbaomercy0@gmail.com>
2026-06-03 10:31:23 +0300

    test(token): add error case coverage for generateAuthToken
    - Add missing require('should') and require('nock') imports
    - Move SDK init into a before block (consistent with other test files)
    - Add test: rejects when API returns token 'None'
    - Add test: rejects on network error

--- test/token.js
+++ test/token.js
@@ -1,13 +1,20 @@
 'use strict'
 
+require('should')
 const joi = require('joi')
+const nock = require('nock')
 const fixtures = require('./fixtures')
 
-describe('Token', function () {
-  it('generates auth token', function (done) {
-    const AfricasTalking = require('../lib')(fixtures.TEST_ACCOUNT)
+let token
 
-    const p = AfricasTalking.TOKEN.generateAuthToken()
+describe('Token', function () {
+  before(function () {
+    const AfricasTalking = require('../lib')(fixtures.TEST_ACCOUNT)
+    token = AfricasTalking.TOKEN
+  })
+
+  it('generates auth token', function (done) {
+    const p = token.generateAuthToken()
     joi.assert(p, joi.object().instance(Promise))
 
     p.then(function (resp) {
@@ -16,5 +23,21 @@ describe('Token', function () {
       done()
     })
       .catch(done)
+  })
+
+  it('rejects when API returns token: None', function () {
+    nock('https://api.sandbox.africastalking.com')
+      .post('/auth-token/generate')
+      .reply(200, { token: 'None', description: 'Invalid credentials' })
+
+    return token.generateAuthToken().should.be.rejected()
+  })
+
+  it('rejects on network error', function () {
+    nock('https://api.sandbox.africastalking.com')
+      .post('/auth-token/generate')
+      .replyWithError('Network failure')
+
+    return token.generateAuthToken().should.be.rejected()
   })
 })
